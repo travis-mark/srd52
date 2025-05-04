de3f2bb75f0e7bbe0656a22c59a4bcae621b690e
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2025-05-03 21:47:15 -0400

    clojure -Tnew app

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tl/srd52
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/tl/srd52_test.clj
@@ -0,0 +1,7 @@
+(ns tl.srd52-test
+  (:require [clojure.test :refer :all]
+            [tl.srd52 :refer :all]))
+
+(deftest a-test
+  (testing "Sample passing test."
+    (is (= 1 1))))
