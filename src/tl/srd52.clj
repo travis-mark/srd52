235312a4de0bf4e704c3ff7090d5994ea82df2c4
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2025-05-03 22:04:43 -0400

    Ability Modifiers (table lookup)

--- src/tl/srd52.clj
+++ src/tl/srd52.clj
@@ -10,3 +10,15 @@
   "I don't do a whole lot ... yet."
   [& args]
   (greet {:name (first args)}))
+
+(def -ability-modifier-vector
+  [-5 -4 -4 -3 -3 -2 -2 -1 -1 0 0 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10])
+(defn ability-modifier 
+  "SRD 5.2 pgs. 5-6
+   
+   Each ability has a modifier that you apply whenever you make a D20 Test with that ability. 
+   An ability modifier is derived from its score, as shown in the Ability Modifiers table."
+  [score]
+  (if (<= 1 score 30)
+    (-ability-modifier-vector (- score 1))
+    (throw (IllegalArgumentException. (str "Score must be between 1 and 30, got: " score)))))
